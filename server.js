71b0c73986c6775766bbd700d26962f8596ba51c
Vincent <isfcikiller@msn.com>
2019-10-09 07:08:26 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const i18n = require('i18n');
+
+// Configure & Run the http server
+const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname)) // TODO maybe move to static folder
+
+i18n.configure({
+  locales:['en', 'de'],
+  directory: __dirname + '/locales'
+});
+app.use(i18n.init);
+
+app.get('/', function(req, res) {
+  res.setLocale('de');
+  res.render('index.ejs');
+});
+
+app.get('/en', function(req, res) {
+  res.setLocale('en');
+  res.render('index.ejs');
+});
+
+app.get('/privacy', function(req, res) {
+  res.setLocale('de');
+  res.render('privacy_de.ejs');
+});
+
+app.get('/en/privacy', function(req, res) {
+  res.setLocale('en');
+  res.render('privacy_en.ejs');
+});
+
+app.listen(4004, () => {
+  console.log('HTTP server running on port 4004');
+});
