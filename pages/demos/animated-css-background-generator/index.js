9f73ce938ab414d163505e8c68d6b7587327d6e1
Vincent <isfcikiller@msn.com>
2019-11-17 10:58:05 +0100

    add navigation

--- pages/demos/animated-css-background-generator/index.js
+++ pages/demos/animated-css-background-generator/index.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Head } from '../../../components'
 import { Background1 } from './components'
 import * as S from './styled'
 
-const Home = () => (
-    <S.Container>
-        <Head
-            title="Animated CSS Background Generator"
-            link="https://vincentwill.com/demos/animated-css-background-generator"
-            image="TODO"
-            description="A collection of pure css animated backgrounds with the possibility to customize"
-            // TODO date={d}
-            isArticle={true}
-        />
+const Demo = () => {
+    const [activeBg, changeBg] = useState(0);
+    return (
+        <S.Container>
+            <Head
+                title="Animated CSS Background Generator"
+                link="https://vincentwill.com/demos/animated-css-background-generator"
+                image="TODO"
+                description="A collection of pure css animated backgrounds with the possibility to customize"
+                // TODO date={d}
+                isArticle={true}
+            />
 
-        <Background1 />
-    </S.Container>
-)
+            <S.Navigation>
+                <a
+                    onClick={() => changeBg(0)}
+                    className={activeBg === 0 ? 'active' : ''}
+                >
+                    Demo 1
+                </a>
+                <a
+                    onClick={() => changeBg(1)}
+                    className={activeBg === 1 ? 'active' : ''}
+                >
+                    Demo 2
+                </a>
+            </S.Navigation>
 
-export default Home
+            { activeBg === 0 && <Background1 /> }
+        </S.Container>
+    )
+}
+
+export default Demo
--- pages/demos/animated-css-background-generator/styled.js
+++ pages/demos/animated-css-background-generator/styled.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.div``
+export const Navigation = styled.nav`
+    position: relative;
+    z-index: 2;
+    padding: 20px 20px 0;
 
+    a {
+        cursor: pointer;
+        color: #fff;
+        margin-left: 20px;
+        text-decoration: none;
+
+        &:first-child {
+            margin-left: 0;
+        }
+
+        &.active {
+            text-decoration: underline;
+        }
+    }
 `
