40f92f0e16597c5f6e74adc96f0352ea25565afd
Vincenius <vincet.will@vaitrade.de>
2020-01-05 10:41:12 +0100

    move commento script to head

--- components/Comments/index.js
+++ components/Comments/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Comments = props => (
     <div>
       <div id="commento"></div>
-      <script src="https://cdn.commento.io/js/commento.js"></script>
     </div>
 )
 
